5172f50821f8965d8fdff9dd5f4e46ddc0c95e37
mystrader <mystrader@gmail.com>
2015-09-22 02:14:39 -0300

    finalizando a aula 07

--- aula_angulardevmedia/js/app07Controller.js
+++ aula_angulardevmedia/js/app07Controller.js
@@ -3,8 +3,40 @@ app.controller("aula07Controller", function($scope){
 
         $scope.pessoas = [];
         $scope.pessoas.push(
-            { nome: "marcus henrique"}
+            { nome: "marcus henrique", idade: 31, status: false }            	
           );
+        $scope.pessoas.push(
+            { nome: "Paulo", idade: 31, status: false }            	
+          );
+        $scope.pessoas.push(
+            { nome: "marcus henrique", idade: 31, status: false }            	
+          );
+        $scope.pessoas.push(
+            { nome: "Thiago", idade: 31, status: false }            	
+          );
+        $scope.pessoas.push(
+            { nome: "Mateus", idade: 31, status: false }            	
+          );
+        $scope.pessoas.push(
+            { nome: "João", idade: 31, status: false }            	
+          );
+        $scope.pessoas.push(
+            { nome: "Pedro", idade: 31, status: false }            	
+          );
+        console.log($scope.pessoas);
 
-               console.log($scope.pessoas);
+
+
+			$scope.adicionaPessoa = function(){
+				var nome = document.getElementById("nomepessoa");
+				var idade = document.getElementById("idadepessoa");
+
+				$scope.pessoas.push({
+					nome: nome.value, idade : idade.value
+				});
+
+				nome.value = "";
+				idade.value = "";
+			}
+
 });
