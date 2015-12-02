d7b9844f1a26c8676d4c5666c364210de29f730e
mystrader <mystrader@gmail.com>
2015-12-02 02:35:29 -0200

    mufd

--- CursoAlura/Angular/01-alurapic/public/js/controllers/fotos-controller.js
+++ CursoAlura/Angular/01-alurapic/public/js/controllers/fotos-controller.js
@@ -1,3 +1,7 @@
-angular.module('alurapic').controller('FotosController',function(){
+angular.module('alurapic').controller('FotosController', function($scope){
 
+		$scope.foto = {
+			titulo : 'Leão',
+			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'	
+		};
 });
