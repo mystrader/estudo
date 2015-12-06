e63b202b144ca3d0566280d4d5b0934b68de905c
mystrader <mystrader@gmail.com>
2015-12-06 18:03:00 -0200

    aula 2 parte1

--- CursoAlura/Angular/01-alurapic/public/js/controllers/fotos-controller.js
+++ CursoAlura/Angular/01-alurapic/public/js/controllers/fotos-controller.js
@@ -1,7 +1,18 @@
 angular.module('alurapic').controller('FotosController', function($scope){
 
-		$scope.foto = {
+		$scope.fotos = [
+		{
 			titulo : 'Leão',
 			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'	
-		};
+		},
+		{
+			titulo : 'Leão 2',
+			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'	
+		},
+		{
+			titulo : 'Leão 3',
+			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'	
+		},
+	];
+
 });
