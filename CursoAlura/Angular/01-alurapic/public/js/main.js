395226eb786a4f8d25a4d8657ad23cd334df21b5
mystrader <mystrader@gmail.com>
2016-02-01 01:41:58 -0200

    aula 7 completa

--- CursoAlura/Angular/01-alurapic/public/js/main.js
+++ CursoAlura/Angular/01-alurapic/public/js/main.js
@@ -1,22 +1,23 @@
-// public/js/main.js
+angular.module('alurapic', ['minhasDiretivas','ngAnimate', 'ngRoute'])
+	.config(function($routeProvider, $locationProvider) {
 
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
-    .config(function($routeProvider, $locationProvider) {
+		$locationProvider.html5Mode(true);
 
-        $locationProvider.html5Mode(true);
+		$routeProvider.when('/fotos', {
+			templateUrl: 'partials/principal.html',
+			controller: 'FotosController'
+		});
 
-        $routeProvider.when('/fotos', {
-            templateUrl: 'partials/principal.html',
-            controller: 'FotosController'
-        });
+		$routeProvider.when('/fotos/new', {
+			templateUrl: 'partials/foto.html',
+			controller: 'FotoController'
+		});
 
-        // adicionando a propriedade controller que faltava. 
+		$routeProvider.when('/fotos/edit/:fotoId', {
+			templateUrl: 'partials/foto.html',
+			controller: 'FotoController'
+		});
 
-        $routeProvider.when('/fotos/new', {
-            templateUrl: 'partials/foto.html',
-            controller: 'FotoController'
-        });
+		$routeProvider.otherwise({redirectTo: '/fotos'});
 
-        $routeProvider.otherwise({redirectTo: '/fotos'});
-
-    });
+	});
