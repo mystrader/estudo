a38b7ef45cfce5e4cc7fdad9a6acdec73aa163b9
Marcus Cunha <mystrader@gmail.com>
2017-03-12 22:05:44 -0300

    subindo a aula de operadores logicos

--- ALURA/javascript/Alura_js-programando-na-linguagem-web/introducao-javascript/js/principal.js
+++ ALURA/javascript/Alura_js-programando-na-linguagem-web/introducao-javascript/js/principal.js
@@ -1,5 +1,3 @@
-// principal.js
-
 var paciente = document.querySelector("#primeiro-paciente");
 
 var tdPeso = paciente.querySelector(".info-peso");
@@ -8,18 +6,24 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-// selecionando a coluna do IMC
 var tdImc = paciente.querySelector(".info-imc");
 
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-
-
-if (peso < 0 ){
-    console.log("peso inválido")
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
 }
 
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+}
 
-
-var imc = peso / (altura * altura);
-
-tdImc.textContent = imc;
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
