4fe883c3d60e10a1f08a5918c20a808a4e13a1a8
Marcus mystrader <mystrader@gmail.com>
2017-08-01 20:49:10 -0300

    ultima alteração no projeto UDEMY/react

---
+++ YEOMAN/generator-angular/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name generatorAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the generatorAngularApp
+ */
+angular.module('generatorAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
