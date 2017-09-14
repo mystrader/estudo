00ac985924cadcfc2174ebca24cd495fab08b219
Marcus mystrader <mystrader@gmail.com>
2017-09-14 02:06:55 -0300

    subindo exercício de map js

---
+++ exerciciosJS/foreach_nevermore/1_MAP/Aula2_map.js
@@ -0,0 +1,51 @@
+
+/*===========================
+// Aula 02 - Map Exercises
+================================*/
+
+const {videos} = require("./mapData");
+
+//const videoWithIdAndTitle  = videos.map(video => ({
+//	id: video.id,
+//	title: video.title
+//}));
+//console.log((videoWithIdAndTitle));
+
+
+Array.prototype.map  = function (modifierFunction) {
+	let result = [];
+	this.forEach(item => result.push(modifierFunction(item)));
+	return result;
+};
+
+const toPairsOfIdAndTitle = video =>  ({
+	id: video.id,
+	title: video.title, 
+});
+
+const videoWithIdAndTitle = videos.map(toPairsOfIdAndTitle);
+
+console.log((videoWithIdAndTitle));
+
+/*
+ -- Remember tips
+
+1) listando 
+for (var i = 0; i >= names.length; i++) {
+	names[i]	
+}
+names.forEach( name => console.log (name));
+
+2)
+let videoWithIdAndTitle = [];
+videos.forEach(video => videoWithIdAndTitle.push({
+	id: video.id,
+	title: video.title, 
+	uri: video.uri, 
+}));
+console.log((videoWithIdAndTitle));
+
+
+
+*/
+
